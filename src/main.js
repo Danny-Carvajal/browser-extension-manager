850bec0d71877546fa5bea6bd6bbe9edb06e5cb1
Danny-Carvajal <dannycq2112@gmail.com>
2025-11-09 19:45:11 -0600

    añadidos archivos html css y script

--- src/main.js
+++ src/main.js
@@ -1,24 +1,66 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const themeToggle = document.querySelector(".theme-toggle");
+const body = document.body;
+const logo = document.querySelector(".logo-wrapper");
+const themeIcon = document.querySelector(".theme-icon");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme === "light") {
+  body.classList.add("light-mode");
+  logo.src = "../assets/images/logo-dark.svg";
+  themeIcon.src = "../assets/images/icon-moon.svg";
+}
 
-setupCounter(document.querySelector('#counter'))
+
+themeToggle.addEventListener("click", () => {
+  body.classList.toggle("light-mode");
+  const isLight = body.classList.contains("light-mode");
+
+  logo.src = isLight
+    ? "../assets/images/logo-dark.svg"
+    : "../assets/images/logo-light.png";
+  themeIcon.src = isLight
+    ? "../assets/images/icon-moon.svg"
+    : "../assets/images/icon-sun.svg";
+
+  
+  localStorage.setItem("theme", isLight ? "light" : "dark");
+});
+
+const filterButtons = document.querySelectorAll(".filter-btn");
+const cards = document.querySelectorAll(".extension-card");
+
+filterButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    
+    filterButtons.forEach((b) => b.classList.remove("active"));
+    
+    btn.classList.add("active");
+
+    const filter = btn.dataset.filter;
+
+    cards.forEach((card) => {
+      const toggle = card.querySelector(".toggle-switch input");
+      const isActive = toggle.checked;
+
+      
+      if (filter === "all") {
+        card.style.display = "flex";
+      } else if (filter === "active" && isActive) {
+        card.style.display = "flex";
+      } else if (filter === "inactive" && !isActive) {
+        card.style.display = "flex";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
+});
+
+
+cards.forEach((card) => {
+  const toggle = card.querySelector(".toggle-switch input");
+  toggle.addEventListener("change", () => {
+    const activeFilter = document.querySelector(".filter-btn.active");
+    if (activeFilter) activeFilter.click();
+  });
+});
